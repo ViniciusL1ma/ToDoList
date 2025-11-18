ffcd2bbd05bc83943c67f4e61416209b66d06975
Vinicius <Vvini3881@gmail.com>
2025-11-18 20:33:10 -0300

    ambiente para criação da webapi criado

---
+++ ProjetoToDoList/models/Lista.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoToDoList.models
+{
+    public class Lista
+    {
+        public int MyProperty { get; set; }
+        public string Tarefa { get; set; } 
+    }
+}
