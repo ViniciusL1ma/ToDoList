81c5d9e955836d22a48cd4be4372a25b4bcbab75
Vinicius <Vvini3881@gmail.com>
2025-11-21 21:36:49 -0300

    frontend iniciado.

--- Frontend/src/Page/Home/index.js
+++ Frontend/src/Page/Home/index.js
@@ -1,11 +1,36 @@
-import './App.css';
+import './index.scss';
+//import { useState } from "react";
+//import axios from 'axios';
 
-function Index() {
+export default function Index() {
+  
+  
+  
+  
   return (
-    <div className="Container-All">
-     
-    </div>
+    <section className="container-All">
+        <section classsName="container-Center">
+          <div className='cabecalho'>
+
+            <h2 className='name-Program'>To Do List</h2>
+            <button className='create'> Criar</button>
+            <button className='edit'>Editar</button>
+            <button className='delete'>Deletar</button>
+
+          </div>
+          <div className='collumns-List'>
+            <h2 className='name-task'> Tarefa</h2>
+            <h2 className='obs-task'>Observação</h2>
+            <h2 className='prevision-task'>Conclusao</h2>
+
+          </div>
+
+        </section>
+
+
+
+    </section>
   );
 }
 
-export default Index;
+
